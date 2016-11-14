d13e054e950c488e73dc737f7df77ddf02cee388
izumin5210 <masayuki@izumin.info>
2016-11-14 12:59:15 +0900

    Apply styles for whole app layout

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,9 +4,15 @@ import Timer from "../components/Timer";
 
 export default function App() {
   return (
-    <Timer
-      remainTimeInMillis={1200000}
-      totalTimeInMillis={1500000}
-    />
+    <div className="App">
+      <header className="App__header" />
+      <main className="App__main">
+        <Timer
+          remainTimeInMillis={1200000}
+          totalTimeInMillis={1500000}
+        />
+      </main>
+      <footer className="App__footer" />
+    </div>
   );
 }
