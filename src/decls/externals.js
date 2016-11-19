a5e893fe82a7268e0677fef64384ea664e1a471e
izumin5210 <masayuki@izumin.info>
2016-11-20 02:31:44 +0900

    Add sample testcase

---
+++ src/decls/externals.js
@@ -0,0 +1,10 @@
+declare var it: any;
+declare var xit: any;
+declare var context: any;
+declare var describe: any;
+declare var assert: any;
+
+declare module "enzyme" {
+  declare function shallow(component: any): any;
+}
+
