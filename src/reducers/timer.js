8fc3b8806972edf83f00391fe071a173a48abaa1
izumin5210 <masayuki@izumin.info>
2016-11-23 11:22:19 +0900

    Refactor reducers

--- src/reducers/timer.js
+++ src/reducers/timer.js
@@ -1,38 +1,33 @@
+/* @flow */
+
 import {
   Iteration,
 } from "../entities";
 
-function nextId(state) {
-  const latestItr = state.get("iterations").maxBy(itr => itr.id);
+import {
+  State,
+} from "../models";
+
+function nextId(state: State): number {
+  const latestItr = state.iterations.maxBy(itr => itr.id);
   return (latestItr === undefined) ? 1 : (latestItr.id + 1);
 }
 
-export function startTimer(state) {
-  let nextItr;
-  if (state.get("timer").hasStarted) {
-    const id = state.get("timer").currentIterationId;
-    const itr = state.get("iterations").find(v => v.id === id);
-    nextItr = (itr === undefined) ? Iteration.createFirst(nextId(state)) : itr.next;
+export function startTimer(state: State): State {
+  let nextItr: Iteration;
+  if (state.timer.hasStarted()) {
+    const id = state.timer.currentIterationId;
+    const itr: Iteration = state.iterations.find(v => v.id === id);
+    nextItr = itr.next();
   } else {
     nextItr = Iteration.createFirst(nextId(state));
   }
 
   return state
-    .update("iterations", v => v.push(nextItr))
-    .update("timer", v => v.setIteration(nextItr));
+    .set("iterations", state.iterations.push(nextItr))
+    .set("timer", state.timer.updateIteration(nextItr));
 }
 
-export function stopTimer(state) {
-  return state.update("timer", v => v.stop());
-}
-
-export function checkTimer(state, update) {
-  const timer = state.get("timer");
-  if (timer.hasStarted) {
-    const id = timer.currentIterationId;
-    const itr = state.get("iterations").find(v => v.id === id);
-    if (itr.isFinished) {
-      update(startTimer);
-    }
-  }
+export function stopTimer(state: State): State {
+  return state.set("timer", state.timer.stop());
 }
