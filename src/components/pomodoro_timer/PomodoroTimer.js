7099d0884369d37e6d080b357253e1f36aa0733d
izumin5210 <masayuki@izumin.info>
2016-11-23 11:22:47 +0900

    Keep states in renderer and refactor components

--- src/components/pomodoro_timer/PomodoroTimer.js
+++ src/components/pomodoro_timer/PomodoroTimer.js
@@ -1,10 +1,9 @@
-import React, { Component, PropTypes } from "react";
+/* @flow */
+import React, { Component } from "react";
 import { dispatcher } from "react-dispatcher-decorator";
-import moment         from "moment";
 
 import {
-  IterationType,
-  TimerType,
+  Iteration,
 } from "../../entities";
 
 import {
@@ -12,117 +11,69 @@ import {
   ACTION_TIMER_STOP,
 } from "../../settings/constants";
 
-import tickingSound from "../../assets/audios/ticking.mp3";
-import finishSound  from "../../assets/audios/finish.mp3";
+/*
+ * FIXME
+ * import tickingSound from "../../assets/audios/ticking.mp3";
+ * import finishSound  from "../../assets/audios/finish.mp3";
+ */
 
 import TimerInner from "./TimerInner";
 
+type Props = {
+  iteration: ?Iteration;
+};
+
+// FIXME: I want to add align option to flowtype/space-after-type-colon rule...
+/* eslint-disable no-multi-spaces */
+type State = {
+  intervalId:         ?number;
+  remainTimeInMillis: number;
+};
+/* eslint-enable */
+
+const initialState: State = {
+  intervalId:         undefined,
+  remainTimeInMillis: 0,
+};
+
 @dispatcher
 export default class PomodoroTimer extends Component {
-  static propTypes = {
-    timer: PropTypes.instanceOf(TimerType).isRequired,
-    iterations: PropTypes.arrayOf(
-      PropTypes.instanceOf(IterationType),
-    ),
-  };
 
-  constructor(props) {
+  constructor(props: Props) {
     super(props);
-    this.state = {
-      intervalId: undefined,
-      remainTimeInMillis: 0,
-    };
+    this.state = initialState;
   }
+
+  state: State;
 
   componentDidMount() {
     this.checkUpdates(this.props, this.state);
   }
 
-  componentWillUpdate(props, state) {
+  componentWillUpdate(props: Props, state: State) {
     this.checkUpdates(props, state);
   }
 
   componentWillUnmount() {
-    if (this.state.intervalId !== undefined) {
+    if (this.state.intervalId != null) {
       clearInterval(this.state.intervalId);
     }
   }
 
   onBtnPlayClick() {
-    if (this.hasStarted) {
+    if (this.hasStarted()) {
       this.stop();
     }
-    this.context.dispatch(this.hasStarted ? ACTION_TIMER_STOP : ACTION_TIMER_START);
+    this.context.dispatch(this.hasStarted() ? ACTION_TIMER_STOP : ACTION_TIMER_START);
   }
 
-  checkUpdates(props, state) {
-    const { timer, iterations } = props;
-    const currentItr = iterations.find(v => v.id === timer.currentIterationId);
-    if (state.intervalId === undefined && currentItr !== undefined) {
-      const intervalId = setInterval(() => this.refresh(), 1000);
-      this.setState({ intervalId });
-    }
-  }
-
-  refresh() {
-    const { timer, iterations } = this.props;
-    const itr = iterations.find(v => v.id === timer.currentIterationId);
-    if (itr !== undefined) {
-      if (this.isWorking) {
-        this.tickingSoundEl.play();
-      }
-      const stoppedAt = moment(itr.startedAt).add(itr.totalTimeInMillis, "ms");
-      if (stoppedAt.isBefore()) {
-        this.finishSoundEl.play();
-        this.stop();
-      } else {
-        const duration = moment.duration(stoppedAt - moment());
-        this.setState({ remainTimeInMillis: duration.asMilliseconds() });
-      }
-    }
-  }
-
-  stop() {
-    if (this.state.intervalId !== undefined) {
-      clearInterval(this.state.intervalId);
-    }
-    this.setState({
-      intervalId: undefined,
-      remainTimeInMillis: 0,
-    });
-  }
-
-  get currentIteration() {
-    const { timer, iterations } = this.props;
-    return iterations.find(v => v.id === timer.currentIterationId);
-  }
-
-  get hasStarted() {
-    return this.props.timer && (this.props.timer.currentIterationId !== undefined);
-  }
-
-  get isWorking() {
-    return this.hasStarted && this.currentIteration.isWorking;
-  }
-
-  get totalTimeInMillis() {
-    return this.hasStarted ? this.currentIteration.totalTimeInMillis : 0;
-  }
-
-  get name() {
-    let name = "";
-    if (this.hasStarted) {
-      name = this.hasStarted && `${this.currentIteration.state}(${this.currentIteration.count})`;
-    }
-    return name;
-  }
-
-  get gradients() {
+  getGradients(): Array<string> {
     // FIXME: Should import from settings/constans.css
     const colorText = "#f9f9f9";
-    const bg = this.isWorking ? "#a54c38" : "#5f8e58";
+    const bg = this.isWorking() ? "#a54c38" : "#5f8e58";
 
-    const r = this.hasStarted ? (1 - (this.state.remainTimeInMillis / this.totalTimeInMillis)) : 0;
+    const itr = this.props.iteration;
+    const r = (itr != null) ? (1 - (this.state.remainTimeInMillis / itr.totalTimeInMillis())) : 0;
     if (r < 0.5) {
       const angle = ((r / 0.5) * 180) + 90;
       return [
@@ -138,29 +89,92 @@ export default class PomodoroTimer extends Component {
     ];
   }
 
+  checkUpdates(props: Props, state: State) {
+    const itr = props.iteration;
+    if (state.intervalId == null && itr != null && !itr.isFinished()) {
+      const intervalId = setInterval(() => this.refresh(), 1000);
+      const remainTimeInMillis = itr.totalTimeInMillis();
+      this.setState({ intervalId, remainTimeInMillis });
+    }
+  }
+
+  refresh() {
+    const itr = this.props.iteration;
+    if (itr != null) {
+      if (itr.isWorking()) {
+        /*
+         * FIXME
+         * this.tickingSoundEl.play();
+         */
+      }
+      this.setState({ remainTimeInMillis: itr.remainTimeInMillis() });
+      if (itr.isFinished()) {
+        /*
+         * FIXME
+         * this.finishSoundEl.play();
+         */
+        this.stop();
+        this.context.dispatch(ACTION_TIMER_START);
+      }
+    }
+  }
+
+  stop() {
+    if (this.state.intervalId != null) {
+      clearInterval(this.state.intervalId);
+      this.setState(initialState);
+    }
+  }
+
+  hasStarted(): boolean {
+    return this.props.iteration != null;
+  }
+
+  isWorking(): boolean {
+    const itr = this.props.iteration;
+    return (itr != null) && itr.isWorking();
+  }
+
+  totalTimeInMillis(): number {
+    const itr = this.props.iteration;
+    return (itr != null) ? itr.totalTimeInMillis() : 0;
+  }
+
+  name(): string {
+    const itr = this.props.iteration;
+    return (itr != null) ? `${itr.state}(${itr.count})` : "";
+  }
+
+  props: Props;
+
   render() {
     return (
       <div
         className="PomodoroTimer"
         style={{
-          backgroundImage: this.gradients.join(", "),
+          backgroundImage: this.getGradients().join(", "),
         }}
       >
         <TimerInner
-          name={this.name}
-          hasStarted={this.hasStarted}
-          isWorking={this.isWorking}
+          name={this.name()}
+          hasStarted={this.hasStarted()}
+          isWorking={this.isWorking()}
           remainTimeInMillis={this.state.remainTimeInMillis}
           onBtnPlayClick={() => this.onBtnPlayClick()}
         />
-        <audio
-          src={tickingSound}
-          ref={(ref) => { this.tickingSoundEl = ref; }}
-        />
-        <audio
-          src={finishSound}
-          ref={(ref) => { this.finishSoundEl = ref; }}
-        />
+        {
+        /*
+         * FIXME: Should use WebAudio API
+         * <audio
+         *   src={tickingSound}
+         *   ref={(ref) => { this.tickingSoundEl = ref; }}
+         * />
+         * <audio
+         *   src={finishSound}
+         *   ref={(ref) => { this.finishSoundEl = ref; }}
+         * />
+         */
+        }
       </div>
     );
   }
