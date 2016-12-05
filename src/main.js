bc336d98f9110b814ca229ad285ad35d03ed642b
izumin5210 <masayuki@izumin.info>
2016-12-05 23:09:33 +0900

    Enable disable-renderer-backgrounding switch

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 /* @flow */
+/* eslint-disable import/no-extraneous-dependencies */
+import { app } from "electron";
+/* eslint-enable */
 import menubar from "menubar";
 
+app.commandLine.appendSwitch("disable-renderer-backgrounding");
+
 const mb = menubar({
+  app,
   dir:           __dirname,
   icon:          `${__dirname}/icon.png`,
   preloadWindow: true,
