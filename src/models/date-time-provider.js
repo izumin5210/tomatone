26452a69fd9cebaa76192b6e81bb7cc705e4255d
izumin5210 <masayuki@izumin.info>
2016-12-08 03:36:17 +0900

    Create DateTimeProvider interface and its implementation

---
+++ src/models/date-time-provider.js
@@ -0,0 +1,12 @@
+/* @flow */
+/* eslint-disable import/prefer-default-export */
+
+export interface DateTimeProvider {
+  nowInMilliSeconds(): number;
+}
+
+export function nowInMilliSeconds(): number {
+  return Date.now();
+}
+
+/* eslint-disable */
--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,18 @@
 /* @flow */
-import SoundPlayer from "./sound-player";
-import State       from "./state";
+import * as dateTimeProvider from "./date-time-provider";
+import SoundPlayer      from "./sound-player";
+import State            from "./state";
+
+/* eslint-disable no-duplicate-imports */
+import type { DateTimeProvider } from "./date-time-provider";
+/* eslint-enable */
 
 export {
+  dateTimeProvider,
   SoundPlayer,
   State,
+};
+
+export type {
+  DateTimeProvider,
 };
