bfc3dc0d9b72dfcf8b99ca7efa445d5f25538c88
izumin5210 <masayuki@izumin.info>
2017-01-15 18:59:47 +0900

    feat(HistoryView): Show history that is included in the selected category or its child categories

--- src/components/iteration-list/IterationList.js
+++ src/components/iteration-list/IterationList.js
@@ -5,15 +5,13 @@ import { Map }              from "immutable";
 
 import IterationItem from "./IterationItem";
 
-import {
-  Category,
+import type {
   Iteration,
   Task,
 } from "../../entities";
 
 /* eslint-disable no-multi-spaces */
 type Props = {
-  currentCategory: Category;
   iterations:      Map<number, Iteration>;
   tasks:           Map<number, Task>;
 };
@@ -22,9 +20,8 @@ type Props = {
 export default class IterationList extends Component {
 
   getListItems() {
-    const { currentCategory, iterations, tasks } = this.props;
+    const { iterations, tasks } = this.props;
     return iterations
-      .filter(({ taskId }) => currentCategory.includes(tasks.get(taskId)))
       .sortBy(itr => -itr.id)
       .groupBy(itr => moment(itr.startedAt).format("YYYY-MM-DD"))
       .map((itrs, date) => {
